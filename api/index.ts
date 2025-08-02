38d04bf6054de87a8370c503cdc23385cf4f574e
Blu <015042005minh@gmail.com>
2025-08-03 02:22:47 +0700

    19

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,6 @@ import { getENVKey } from "./utils";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).send({
-    test: ["BROWSER_LESS_KEY"].map((a) => getENVKey(a)),
+    test: Object.keys(process.env),
   });
 }

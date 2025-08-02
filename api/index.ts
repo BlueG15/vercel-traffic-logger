2982c5d5bdbf8e55d7728c8ec8595ef81433e6cc
Blu <015042005minh@gmail.com>
2025-08-02 23:24:06 +0700

    Added a method

--- api/index.ts
+++ api/index.ts
@@ -5,13 +5,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).send({
     test: [
       "APP_VERSION",
-      "extensionStr",
-      "extensionStr2",
-      "extensionStr3",
-      "extensionStr4",
-      "key_concat_string",
-      "key_length",
-      "total_key_number",
     ].map((a) => getENVKey(a)),
   });
 }
---
+++ api/test.ts
@@ -0,0 +1,37 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { chromium } from 'playwright'
+
+import { Response, cors, getPropertyNameFromReqObject } from "./utils";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const browser = await chromium.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+    const page = await browser.newPage()
+    await page.goto('https://example.com')
+
+    const title = await page.title()
+    const content = await page.content()
+
+    await browser.close()
+
+    
+    //# Handle CORS
+    cors(res);
+    //# Allow caching upto 1 hour
+    res.setHeader("Cache-Control", "max-age=3600, public");
+    res.setHeader("vary", "Accept");
+    
+    res.status(200).json({ title, length: content.length })
+  } catch (err) {
+    const logStr = "AHhhhhh";
+    const sendData = new Response(true, logStr, {
+      error: err.message,
+    });
+
+    console.error(err);
+    res.status(400).send(sendData);
+  }
+}

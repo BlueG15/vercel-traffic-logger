f87da0f8a1eaaad193489c7616d2075137566724
Blu <015042005minh@gmail.com>
2025-08-03 00:04:35 +0700

    Still fixin bugs 5

--- api/test.ts
+++ api/test.ts
@@ -4,7 +4,7 @@ import path = require("path");
 
 const fs = require("fs")
 
-const files = fs.readdirSync('./node_modules/.playwright')
+const files = fs.readdirSync('../node_modules/.playwright')
 
 import { Response, cors, getPropertyNameFromReqObject } from "./utils";
 

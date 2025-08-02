c48061ca72da3204be801448835b8720af85e422
Blu <015042005minh@gmail.com>
2025-08-03 02:01:32 +0700

    16

--- api/test.ts
+++ api/test.ts
@@ -1,20 +1,16 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { chromium } from 'playwright'
-import path = require("path");
-
-const fs = require("fs")
-
-const p = path.join(process.cwd(), "node_modules", "playwright-core") //we look for the folder .local-browsers 
-const files = fs.readdirSync(path.join(p))
+import { chromium } from 'playwright-core'
 
 import { Response, cors, getPropertyNameFromReqObject } from "./utils";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const browser = await chromium.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    })
+
+    //throwaway token, so its fine
+    const browser = await chromium.connectOverCDP(
+      `wss://production-sfo.browserless.io?token=2SnLFcPKJV9OPhwfdc108912e370aa2561772989f03d68765`
+    );
+
     const page = await browser.newPage()
 
     await page.goto('https://example.com')
@@ -31,15 +27,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader("Cache-Control", "max-age=3600, public");
     res.setHeader("vary", "Accept");
     
-    res.status(200).json({ title, length: content.length, path : p, files })
+    res.status(200).json({ title, length: content.length})
   } catch (err) {
     
 
     const logStr = "Aghhhhh";
     const sendData = new Response(true, logStr, {
-      error: err.message,
-      path : p,
-      files
+      error: err.message
     });
 
     res.status(400).send(sendData);

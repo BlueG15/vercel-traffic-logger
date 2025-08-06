7c6c65e504c14e64049291d191099bda143f0be2
Blu <015042005minh@gmail.com>
2025-08-06 12:25:19 +0700

    28

--- api/path.ts
+++ api/path.ts
@@ -3,7 +3,7 @@ const fs = require("fs")
 const os = require("os")
 
 export default async function handler(req: any, res: any) {
-    const files = path.join("vercel");
+    const files = path.join(".");
     const dirs = fs.readdirSync(files);
 
     res.status(200).send(dirs)

25f321a57a983cce16f5d105127cf813585cf8f6
Blu <015042005minh@gmail.com>
2025-08-06 12:29:27 +0700

    29

--- api/path.ts
+++ api/path.ts
@@ -2,8 +2,13 @@ const path = require("path")
 const fs = require("fs")
 const os = require("os")
 
+import {getPropertyNameFromReqObject} from "./utils"
+
 export default async function handler(req: any, res: any) {
-    const files = path.join(".");
+    let num = getPropertyNameFromReqObject(req as any, "num") ?? 1
+    num = Math.max(num, 1)
+
+    const files = path.join(".".repeat(num));
     const dirs = fs.readdirSync(files);
 
     res.status(200).send(dirs)

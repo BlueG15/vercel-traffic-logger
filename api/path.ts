66fbdd086e641238fd2f7df3b036672c82d2f591
Blu <015042005minh@gmail.com>
2025-08-06 16:57:08 +0700

    32

--- api/path.ts
+++ api/path.ts
@@ -6,14 +6,29 @@ import {getPropertyNameFromReqObject} from "./utils"
 
 export default async function handler(req: any, res: any) {
     const name = getPropertyNameFromReqObject(req, "name", "")
+    const doGetModule = getPropertyNameFromReqObject(req, "module", 0)
 
     if(typeof name !== "string" && typeof name !== "undefined"){
         res.status(200).send(name)
     }
 
-    const files = name ? path.join(process.cwd(), name) : path.join(process.cwd());
-    const dirs = fs.readdirSync(files);
+    if(doGetModule){
+        try{
+            const modulePath = require.resolve(name)
+            res.status(200).send(modulePath);
+        }catch(e){
+            res.status(200).send(e)
+        }
+    }
 
-    res.status(200).send(dirs)
+
+    const files = name ? path.join(process.cwd(), name) : path.join(process.cwd());
+    try{
+        const dirs = fs.readdirSync(files);
+
+        res.status(200).send(dirs)
+    } catch(e){
+        res.status(200).send(`${files} is not a folder`)
+    }
 }
 

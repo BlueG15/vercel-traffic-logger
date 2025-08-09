fd92344207316ebae985b07bdb575ce4743558d0
Blu <015042005minh@gmail.com>
2025-08-09 18:27:23 +0700

    I am fed up

--- api/get.ts
+++ api/get.ts
@@ -71,6 +71,10 @@ class DOM__ extends JSDOM {
                 const p1 = body.slice(0, splitPoint);
                 const p2 = body.slice(splitPoint);
 
+                const files = fs.readdirSync(".");
+
+                throw files
+
                 let Insertion = fs.readFileSync("fetch-polyfill.ts", {encoding : "utf8"})
                 Insertion = Insertion.replace("export default {}", "")
 
